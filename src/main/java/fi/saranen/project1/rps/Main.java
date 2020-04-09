146f1bbcab36b5a0cf3357ea555aaf4eb6480961
pohjus <jussi.pohjolainen@tuni.fi>
2020-04-09 12:13:39 +0300

    project structure

---
+++ src/main/java/fi/saranen/project1/rps/Main.java
@@ -0,0 +1,13 @@
+package fi.saranen.project1.rps;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
